e74e5b1c6e5a4a8ef8987c2bc0cd91f86f2af5cd
Ranveer Singh <36342506+Ranveer6699@users.noreply.github.com>
2019-10-30 15:10:13 -0500

    Update position.v

--- position.v
+++ position.v
@@ -95,7 +95,7 @@ module Axis_Position (input clk,  input [3:0] mode_selector, input [3:0] pos_sel
     // 0100 is the defense mode
     // 1000 is the stealth mode
     // The output of the mode multiplexer would be the velocity associated with that mode
-    Mux_4 mode(stealth_speed, defense_speed, attack_speed, 16'b0, mode_selector, velocity_out);  // Add arbitary values for a1, a2 and a3
+    Mux4 mode(stealth_speed, defense_speed, attack_speed, 16'b0, mode_selector, velocity_out);  // Add arbitary values for a1, a2 and a3
 
     Add_sub_rca16 V_adder(1'b0, velocity_out, position, 1'b0, c_out, adder_out); // The adder would ouput the next position in the normal case
     // 4 bit one hot values for the multiplexer position
@@ -103,7 +103,7 @@ module Axis_Position (input clk,  input [3:0] mode_selector, input [3:0] pos_sel
     // 0010 is the normal result which is the sum of the previous position and current velocity/clk * clk = velocity
     // 0100 is the warp speed mode
     // 1000 is a dont care value and should never appear
-    Mux_4 position(16'b1, warp_speed_value, adder_out, 16'b0, pos_selector, position_value);  // Set the warp speed to an arbitary large value // teleportation pretty much
+    Mux4 position(16'b1, warp_speed_value, adder_out, 16'b0, pos_selector, position_value);  // Set the warp speed to an arbitary large value // teleportation pretty much
 
     // Its gonna take the output of the position multiplexer
     DFF Q(clk, position_value, position_out);
