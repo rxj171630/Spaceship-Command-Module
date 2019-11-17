73520203bc522fb227fba49ac269263ab84f409f
austin5627 <austinsharris@gmail.com>
2019-11-17 17:52:34 -0600

    Made more behavorial

--- position.v
+++ position.v
@@ -42,6 +42,18 @@ module Mux4(a3, a2, a1, a0, s, b) ;
 endmodule
 
 
+
+//Adder Module
+module Adder(a, b, out);
+  parameter k = 16;
+
+  input [k-1:0] a;
+  input [k-1:0] b;
+  output [k-1:0] out;
+
+  assign out = a + b;
+endmodule
+
 //This is the module for calculating the position in a single axis
 module Axis_Position (clk, pos_mode, jump_position, velocity);   
 
@@ -55,16 +67,15 @@ module Axis_Position (clk, pos_mode, jump_position, velocity);
   wire [k-1:0] position, next_position;
   wire [k-1:0] adder_out;
   
-  assign adder_out = position + velocity;
-
-   // The adder would ouput the next position in the normal case
+  // The adder would ouput the next position in the normal case
+  Adder add(position, velocity, adder_out);
 
   // 4 bit one hot values for the multiplexer position
   // 0001 is the reset 
   // 0010 is the sublight which is the sum of the current position and velocity
   // 0100 is the jump mode
-  // 1000 is a dont care value and should never appear
-  Mux4 position_mux({{k-1{1'b0}},1'b1}, jump_position, adder_out, {k{1'b0}}, pos_mode, next_position);  // Set the warp speed to an arbitary large value // teleportation pretty much
+  // 1000 is a do nothing
+  Mux4 position_mux(position, jump_position, adder_out, {k{1'b0}}, pos_mode, next_position);  // Set the warp speed to an arbitary large value // teleportation pretty much
 
   // Its gonna take the output of the position multiplexer
   DFF #(k) Q(clk, next_position, position);
@@ -80,7 +91,9 @@ module Position(clk, mode, pos_mode, jump_position, velocity);
   input [3*k-1:0] jump_position;// in the form {X, Y, Z}
   input [3*k-1:0] velocity;// in the form {X, Y, Z}
 
-  reg [3*k-1:0] position;
+  reg [k-1:0] x;
+  reg [k-1:0] y;
+  reg [k-1:0] z;
   
   // Calculating 
   Axis_Position #(k) x_pos(clk, pos_mode, jump_position[`X], velocity[`X]);//Calulates x positon
@@ -88,13 +101,13 @@ module Position(clk, mode, pos_mode, jump_position, velocity);
   Axis_Position #(k) z_pos(clk, pos_mode, jump_position[`Z], velocity[`Z]);//Calulates z position
   always @(*)
       begin
-          position[`X] = x_pos.position;
-          position[`Y] = y_pos.position;
-          position[`Z] = z_pos.position;
+          x = x_pos.position;
+          y = y_pos.position;
+          z = z_pos.position;
       end
 endmodule
 
-module Axis_Velocity(mode, speed, velocity);
+module Axis_Velocity(mode, speed);
   parameter k = 16;
   
   `define STEALTH_OFFSET 3
@@ -104,7 +117,7 @@ module Axis_Velocity(mode, speed, velocity);
 
   input [3:0] mode;
   input [k-1:0] speed;//default speed
-  output [k-1:0] velocity;//adjusted speed
+  wire [k-1:0] velocity;//adjusted speed
 
   //Takes speed and applies offset based on mode
   wire [k-1:0] stealth_speed, defense_speed, attack_speed;
@@ -127,18 +140,17 @@ module Velocity(mode, speed);
 
   input [3:0] mode;
   input [3*k-1:0] speed;//in the form {XSPEED, YSPEED, ZSPEED}
-  wire  [k-1:0] x, y, z;  
-  reg [3*k-1:0] velocity;  
+  reg [k-1:0] x, y, z;  
 
 
-  Axis_Velocity #(k) x_vel(mode, speed[`X], x);
-  Axis_Velocity #(k) y_vel(mode, speed[`Y], y);
-  Axis_Velocity #(k) z_vel(mode, speed[`Z], z);
+  Axis_Velocity #(k) x_vel(mode, speed[`X]);
+  Axis_Velocity #(k) y_vel(mode, speed[`Y]);
+  Axis_Velocity #(k) z_vel(mode, speed[`Z]);
   always @(*)
   begin
-    velocity[`X] = x;
-    velocity[`Y] = y;
-    velocity[`Z] = z;
+    x = x_vel.velocity;
+    y = y_vel.velocity;
+    z = z_vel.velocity;
   end
 endmodule
 
@@ -152,9 +164,9 @@ module TestBench();
   
   reg [3*k-1:0] jump_position;// in the form {X, Y, Z}
   reg [3*k-1:0] speed;// in the form {X, Y, Z}
-
+  
   Velocity #(k) velocity(mode, speed);
-  Position #(k) position(clk, mode, pos_mode, jump_position, velocity.velocity);
+  Position #(k) position(clk, mode, pos_mode, jump_position, {velocity.x, velocity.y, velocity.z});
 
 	//---------------------------------------------
 	//The Display Thread with Clock Control
@@ -182,7 +194,7 @@ module TestBench();
 		forever
 			begin
 				#10
-				$display(" %b |%d|%d|%d|",clk, position.position[`X], position.position[`Y], position.position[`Z]);
+				$display(" %b |%d|%d|%d|",clk, position.x, position.y, position.z);
 			end
 	end	
 
@@ -191,10 +203,10 @@ module TestBench();
 	initial 
 		begin
 			#2 //Offset the Square Wave
-      #10 mode = `RESET; pos_mode = `RESET;
+      #10 mode = `RESET;  pos_mode = `RESET;
       #10 mode = `ATTACK; pos_mode = `NORMAL; speed[`X] = 1; speed[`Y] = 1; speed[`Z] = 1;
 			#50
-      #10 mode = `ATTACK; pos_mode = `JUMP; jump_position[`X] = 100; jump_position[`Y] = 100; jump_position[`Z] = 100;
+      #10 mode = `ATTACK; pos_mode = `JUMP;   jump_position[`X] = 100; jump_position[`Y] = 100; jump_position[`Z] = 100;
       #10 mode = `ATTACK; pos_mode = `NORMAL; speed[`X] = 4; speed[`Y] = 4; speed[`Z] = 4;
 			#50
 			
