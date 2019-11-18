70e2d1e7930c239ab367989333b3b609fc0cbeb3
rxj171630 <rxj171630@utdallas.edu>
2019-11-17 18:52:27 -0600

    Removed a multiplexer, condensed the code even further

--- weapons.v
+++ weapons.v
@@ -86,22 +86,6 @@ module Mux2(a1, a0, s, b) ;
              ({k{s[0]}} & a0) ;
 endmodule
 
-//=============================================
-// 4-Channel, 1-Bit Multiplexer
-//=============================================
-
-module Mux4_1(a3, a2, a1, a0, s, b) ;
- parameter k = 1 ;//1 Bit Wide
- input [k-1:0] a3, a2, a1, a0 ;  // inputs
- input [3:0]   s ; // one-hot select
- output[k-1:0] b ;
- assign b = ({k{s[3]}} & a3) |
-              ({k{s[2]}} & a2) |
-              ({k{s[1]}} & a1) |
-              ({k{s[0]}} & a0) ;
-endmodule
-
-
 //=================================================
 //Run Counter
 //=================================================
@@ -136,7 +120,7 @@ module weapons(input clk, input [3:0]mode_selector, input [8:0]ammo, input loadi
   wire mode;
   reg error;
   reg shoot;
-  Mux4_1 selMode(1'b0, 1'b0, 1'b1, 1'b0, mode_selector, mode);  //0010 is attack mode
+  Mux4 #(1) modeSel(1'b0, 1'b0, 1'b1, 1'b0, mode_selector, mode); //0010 is the attack mode
   ammoCount run(clk, ammo, loadingAmmo, shoot, fireRate);   //run the counter
 
   initial begin
